5a13df0cbf85a328a13d2ae87ab1ecd05f433ca3
Josh Justice <josh.justice@accesscfa.com>
2019-11-27 07:14:13 -0500

    Enforce token-to-user relationship

--- db/migrate/20191122003350_create_doorkeeper_tables.rb
+++ db/migrate/20191122003350_create_doorkeeper_tables.rb
@@ -74,6 +74,6 @@ class CreateDoorkeeperTables < ActiveRecord::Migration[6.0]
 
     # Uncomment below to ensure a valid reference to the resource owner's table
     # add_foreign_key :oauth_access_grants, <model>, column: :resource_owner_id
-    # add_foreign_key :oauth_access_tokens, <model>, column: :resource_owner_id
+    add_foreign_key :oauth_access_tokens, :users, column: :resource_owner_id
   end
 end
