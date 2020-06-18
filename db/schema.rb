24c71346cec38816703296b3b554bb4adea6f23a
Josh Justice <josh.justice@accesscfa.com>
2020-06-18 06:23:16 -0400

    Add deferred fields to todo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_105155) do
+ActiveRecord::Schema.define(version: 2020_06_18_102228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2019_12_17_105155) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "completed_at", precision: 6
     t.datetime "deleted_at", precision: 6
+    t.datetime "deferred_at", precision: 6
+    t.datetime "deferred_until", precision: 6
     t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
