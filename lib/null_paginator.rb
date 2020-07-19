083eb6af701e105956c67eb602c331b3c70c537c
Josh Justice <me@codingitwrong.com>
2020-07-19 10:45:25 -0400

    Fix cops

--- lib/null_paginator.rb
+++ lib/null_paginator.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class NullPaginator < JSONAPI::Paginator
-  def initialize(params)
-  end
+  def initialize(params); end
 
-  def apply(relation, order_options)
+  def apply(relation, _order_options)
     relation
   end
 
