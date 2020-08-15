006506aa8ec7446dc496d6a5d53feb7533df8445
Josh Justice <me@codingitwrong.com>
2020-08-15 06:59:06 -0400

    Set initial sort order to one more than the max

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,13 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :todos
+
+  before_create :set_default_values
+
+  private
+
+  def set_default_values
+    max_sort_order = user.categories.maximum(:sort_order)
+    self.sort_order ||= max_sort_order + 1
+  end
 end
