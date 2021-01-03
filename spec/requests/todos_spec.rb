827034edda18f049716ca4410c42e600b19c1ec3
Josh Justice <me@codingitwrong.com>
2021-01-03 06:43:56 -0500

    Reproduce error sorting in Ruby 3.0

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe 'todos', type: :request do
     expect(response_body['data'].count).to eq(1)
     expect(response_body['data'][0]['attributes']['name']).to eq(todo.name)
   end
+
+  it 'allows sorting todos' do
+    older_todo = FactoryBot.create(:todo, user: user, completed_at: 2.days.ago)
+    newer_todo = FactoryBot.create(:todo, user: user, completed_at: 1.day.ago)
+
+    get '/todos?sort=-completedAt', headers: headers
+
+    expect(response.status).to eq(200)
+    response_body = JSON.parse(response.body)
+
+    expect(response_body['data'].count).to eq(2)
+    expect(response_body['data'][0]['attributes']['name']).to eq(newer_todo.name)
+    expect(response_body['data'][1]['attributes']['name']).to eq(older_todo.name)
+  end
 end
