cdc4d547ce54ead7226f65233ed0522b85ac843a
Josh Justice <me@codingitwrong.com>
2021-01-04 08:11:40 -0500

    Extract private methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,43 +5,8 @@ class TodosController < ApplicationController
   def index
     if is_available_or_tomorrow?
       todos = Todo.status(:available).or(Todo.status(:tomorrow))
-      response = {
-        data: todos.map { |r|
-          {
-            type: 'todos',
-            id: r.id,
-            attributes: {
-              name: r.name,
-              notes: r.notes,
-              'created-at' => time_string(r.created_at),
-              'updated-at' => time_string(r.updated_at),
-              'deferred-at' => time_string(r.deferred_at),
-              'deferred-until' => time_string(r.deferred_until),
-            },
-            relationships: {
-              category: {
-                data: r.category && {type: 'categories', id: r.category.id},
-              },
-            },
-          }
-        },
-        included: todos.map(&:category)
-                       .compact
-                       .uniq
-                       .map { |r|
-                         {
-                           type: 'categories',
-                           id: r.id,
-                           attributes: {
-                             name: r.name,
-                             'sort-order': r.sort_order,
-                           },
-                         }
-                       },
-        meta: {'page-count': 1},
-      }
       headers['Content-Type'] = 'application/vnd.api+json'
-      render json: response
+      render json: serialize_todos(todos)
     else
       super
     end
@@ -53,7 +18,51 @@ class TodosController < ApplicationController
     params[:filter] && params[:filter][:status] == 'available,tomorrow' && params[:include] == 'category'
   end
 
-  def time_string(time)
-    time&.iso8601(3)
+  def serialize_todos(todos)
+    {
+      data: todos.map { |r| serialize_todo(r) },
+      included: included_categories(todos)
+                  .map { |r| serialize_category(r) },
+      meta: {'page-count': 1},
+    }
+  end
+
+  def included_categories(todos)
+    todos.map(&:category).compact.uniq
+  end
+
+  def serialize_todo(r)
+    {
+      type: 'todos',
+      id: r.id,
+      attributes: serialize_attributes(
+        record: r,
+        attributes: %i(name notes created_at updated_at deferred_at deferred_until),
+      ),
+      relationships: {
+        category: {
+          data: r.category && {type: 'categories', id: r.category.id},
+        },
+      },
+    }
+  end
+
+  def serialize_category(r)
+    {
+      type: 'categories',
+      id: r.id,
+      attributes: serialize_attributes(
+        record: r,
+        attributes: %i(name sort_order),
+      ),
+    }
+  end
+
+  def serialize_attributes(record:, attributes:)
+    attributes.to_h { |a|
+      value = record.public_send(a)
+      value.respond_to?(:iso8601) && value = value.iso8601(3)
+      [a.to_s.dasherize, value]
+    }
   end
 end
