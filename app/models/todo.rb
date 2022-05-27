0dee0cbbdd160d3e533ecdd62c5d27651a0830dc
Josh Justice <me@codingitwrong.com>
2022-05-27 16:43:00 -0400

    Fix or disable rubocop-rspec violations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Todo < ApplicationRecord
   belongs_to :user
-  belongs_to :category, required: false
+  belongs_to :category, optional: true
 
   validates :name, presence: true
 
